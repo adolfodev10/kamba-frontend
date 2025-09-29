4e2872299d93ce10ac514babef72783470d22473
adolfodev10 <adolfomanueldev10@gmail.com>
2025-09-29 19:32:59 +0100

    Refactor code structure for improved readability and maintainability

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View>
-      <Text>Adolfo Monteiro Manuel</Text>
-      <Text className="text-xl font-bold text-red-500">Artur Makumba Paulo</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-red-500">
+      <Text className="text-lg font-bold text-white">
+        Adolfo Monteiro Manuel
+      </Text>
+      <Text className="text-xl font-bold text-red-900">
+        Artur Makumba Paulo
+      </Text>
+      <StatusBar style="light" />
     </View>
   );
 }
- 
